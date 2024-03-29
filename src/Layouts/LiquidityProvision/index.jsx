1a575342c032f67357ce8d2ccde4165aced8c1ca
goodness <goodyc474@gmail.com>
2024-03-29 19:36:10 +0100

    fix issues

--- src/Layouts/LiquidityProvision/index.jsx
+++ src/Layouts/LiquidityProvision/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Button } from './../../components/common/Button/index';
 
 function LiquidityProvision() {
   return (
       <div>
           <div>
-              <h5></h5>
+        <h5>Liquidity Provisioning</h5>
+        <h1>Earn by Providing
+          Liquidity</h1>
+        <p>Earn a higher yield by providing liquidity through GammaSwap.
+          Think of our wrapped pools like wrapped tokens.</p>
+        <p>The GammaSwap protocol will directly deposit liquidity into
+         the underlying AMM and provide an LP token to the depositor
+          representing the position.</p>
+        
+        <Button className="bg-gray-500 text-white p-3">Learn More</Button>
               
           </div>
           <div>
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../../components/navBar'
 import Hero from '../../Layouts/Hero'
+import LiquidityProvision from '../../Layouts/LiquidityProvision'
 
 function Home() {
   return (
     <>
       <NavBar />
       <Hero />
+      <LiquidityProvision/>
     </>
   )
 }
